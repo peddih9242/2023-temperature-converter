fc0e9f0c737a7f35754d51bfc943a7ea82132f73
peddih9242 <78389729+peddih9242@users.noreply.github.com>
2023-02-20 11:56:40 +1300

    check input completed

--- 02_check_input.py
+++ 02_check_input.py
@@ -3,19 +3,22 @@ def temp_check(temp_type):
     while not valid:
         try:
             if temp_type == "celsius":
-                response = int(input("Celsius: "))
                 lower_limit = -273
+                response = int(input("Celsius: "))
+                if response > lower_limit:
+                    valid = True
+                else:
+                    print("Please enter a number above {}.".format(lower_limit))
+            elif temp_type == "fahrenheit":
+                lower_limit = -459
+                response = int(input("Fahrenheit: "))
                 if response > lower_limit:
                     valid = True
                 else:
                     print("Please enter a number above {}.".format(lower_limit))
             else:
-                response = int(input("Fahrenheit: "))
-                lower_limit = -459
-                if response > lower_limit:
-                    valid = True
-                else:
-                    print("Please enter a number above {}.".format(lower_limit))
+                print("Error: valid temperature unit not received")
+                return 0
 
         except ValueError:
             print("Please enter a number above {}.".format(lower_limit))
@@ -24,6 +27,7 @@ def temp_check(temp_type):
 
 # main routine
 
-for item in range:
+for item in range(5):
     type_temp = input("What type of temperature? ")
     temp_check(type_temp)
+    print()
